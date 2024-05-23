53872ef8b213d681d89f4a46e01647c4928719e9
Marci <marton.havasreti@urss.hu>
2024-05-23 15:59:29 +0200

    input validation még nincs kész

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  ValidationErrors,
+  ValidatorFn,
+} from '@angular/forms';
+
+// nincs kész
+function letterValidator() {
+  return null;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'hf220524a';
+export class AppComponent implements OnInit {
+  letterInput = new FormControl('', letterValidator);
+  wordToGuess: string = 'acceleration';
+  lettersInWord: string[];
+  lettersGuessed: string[];
+  wordGuessed: string = '_'.repeat(this.wordToGuess.length);
+  letterMap = new Map<string, boolean>();
+
+  submitLetter() {
+    if (!this.letterInput.valid) {
+      return;
+    }
+    let foundMatch: boolean = false;
+    for (let i = 0; i < this.lettersInWord.length; i++) {
+      if (this.lettersInWord[i] === this.letterInput.value) {
+        this.lettersGuessed[i] = this.lettersInWord[i];
+        foundMatch = true;
+      }
+    }
+    this.wordGuessed = this.lettersGuessed.join('');
+    this.letterMap.set(this.letterInput.value, foundMatch);
+  }
+
+  ngOnInit(): void {
+    this.lettersInWord = this.wordToGuess.split('');
+    this.lettersGuessed = this.wordGuessed.split('');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FiddleComponent } from './fiddle/fiddle.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, FiddleComponent],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
